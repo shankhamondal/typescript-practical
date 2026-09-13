792e79d6472f2e7b5c81b7f64461e8e300a903d0
Shankha Mondal <shankhamondal.codes@gmail.com>
2026-09-13 19:11:03 +0530

    practical01.ts

---
+++ practical01.ts
@@ -0,0 +1,9 @@
+let fullName = "Shankha Mondal";
+let age = 20;
+let gender = "Male";
+let phonenumber = 9749025576;
+
+console.log(fullName);
+console.log(age);
+console.log(gender);
+console.log(phonenumber);
